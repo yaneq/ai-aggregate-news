7ae5e73539a75e297db8975e8835d11c1ec78eda
Jan Koeppen <jan@yaneq.com>
2023-06-20 04:22:33 -0300

    make background fixed, add footer

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -8,6 +8,7 @@ import { createApolloClient } from '@/lib/apolloClient'
 import { ApolloProvider, gql, useQuery } from '@apollo/client'
 import { ArticleListQuery } from './ArticleList'
 import { Header } from '@/components/Header'
+import { Footer } from '@/components/Footer'
 
 export default function News() {
   const client = createApolloClient()
@@ -18,19 +19,21 @@ export default function News() {
         <title>News - xonic.io</title>
       </Head>
       <Layout>
-        <div className={styles.gradients}>
+        <div className="flex h-screen flex-col justify-between">
           <Header />
           <div className="mx-10 mt-10  md:mx-auto md:w-2/3 lg:w-1/2">
             <p className="px-10 text-center font-title text-3xl font-semibold">
               News, tailored for you!
             </p>
-            <div className="mt-10 rounded-lg bg-white p-10 shadow-md ">
+            <div className="mb-36 mt-10 rounded-lg bg-white p-10 shadow-md">
               <ApolloProvider client={client}>
                 <ArticleListQuery />
               </ApolloProvider>
             </div>
           </div>
+          <Footer />
         </div>
+        <div className={styles.gradients} />
       </Layout>
     </>
   )
